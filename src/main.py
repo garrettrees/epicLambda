edb4ea1df95d6ca3f37f0e9433bd1b017dcbba10
Garrett Rees <garrettrees@Garretts-MacBook-Pro.local>
2021-02-25 17:08:18 -0700

    Update main.py to fix month logic, add stuff for Byron, boto3 for Parameter Store

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import datetime
 import time
 import os
+import boto3
 
 def make_reservation(event=None, context=None):
     # This script will reserve 7 days from today's date
@@ -26,17 +27,51 @@ def make_reservation(event=None, context=None):
         'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
     chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
 
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-
-    # Set your username, password & target reservation day (of the current month) before running script
-    username = "greesy@gmail.com"  # email address
-    password = "R!d3-0r-d!3"
     today = datetime.datetime.now().strftime("%d")
-    reservation_day = str(int(today) + 7)
-    #driver = webdriver.Chrome("/Users/garrettrees/chromedriver")
     current_month = datetime.datetime.now().strftime("%m")
 
-    # Set implicit wait for the life of the webdriver object
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    '''
+    #Use AWS Parameter Store:
+    def get_parameters(param_name):
+        ssm = boto3.client('ssm', region_name='us-west-2')
+        response = ssm.get_parameters(
+            Names=[
+                param_name,    def get_parameters(param_name):
+        ssm = boto3.client('ssm', region_name='us-west-2')
+        response = ssm.get_parameters(
+            Names=[
+                param_name,
+            ],
+            WithDecryption=True
+        )
+        credentials = response['Parameters'][0]['Value']
+        return credentials
+            ],
+            WithDecryption=True
+        )
+        credentials = response['Parameters'][0]['Value']
+        return credentials
+    '''
+
+    # Set your username, password & target reservation day (of the current month) before running script
+    user = 'garrett'
+    if user == 'byron':
+        username = ""  # email address
+        password = ""
+        user_xpath = "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div[2]/div/ul/li[2]/span/label/span"
+        #username = get_parameters('epic_username_byron')
+        #password = get_parameters('epic_password_byron')
+    else:
+        username = ''
+        password = ''
+        user_xpath = "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div[2]/div/ul/li/span/label/span"
+        #username = get_parameters('epic_username_garrett')
+        #password = get_parameters('epic_password_garrett')
+
+    # Set implicit wait for the life of the driver object
+    # Set implicit wait for the life of the driver object
     driver.implicitly_wait(30)
     # Maximize browser (so all elements are visible)
     driver.maximize_window()
@@ -71,25 +106,26 @@ def make_reservation(event=None, context=None):
     #### Logic to handle reservations made less than 7 days from the end of the current month ####
     go_to_next_month = driver.find_element_by_xpath(
         "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[1]/button[2]")
-    if (current_month == "1" or "3" or "5" or "7" or "8" or "10" or "12") and today >= "25":
+    if current_month == "01" or current_month == "03" or current_month == "05" or current_month == "07" or current_month == "08" or current_month == "10" or current_month == "12" and today >= "25":
         reservation_day = str(7 - (31 - (int(today))))
         go_to_next_month.click()
-    elif current_month == "2" and today >= "22":
+    elif current_month == "02" and today >= "22":
         reservation_day = str(7 - (28 - (int(today))))
         go_to_next_month.click()
-    elif (current_month == "4" or "6" or "9" or "11") and today >= "24":
+    elif current_month == "04" or current_month == "06" or current_month == "09" or current_month == "11" and today >= "24":
         reservation_day = str(7 - (30 - (int(today))))
         go_to_next_month.click()
     else:
         reservation_day = str(int(today) + 7)
+        go_to_next_month.click()
+
     # Select Date
     desired_date = driver.find_element_by_xpath(
         "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[4]/button[" + reservation_day + "]")
     desired_date.click()
 
     # Assign Pass Holders
-    pass_holder = driver.find_element_by_xpath(
-        "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div[2]/div/ul/li/span/label/span")
+    pass_holder = driver.find_element_by_xpath(user_xpath)
     pass_holder.click()
     assign_button = driver.find_element_by_xpath(
         "/html/body/div[3]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div[2]/div/div[3]/button[2]")
